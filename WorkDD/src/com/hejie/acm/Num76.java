3b426dfb1cc288098b3d63de1eab3d8a298149ce
JustCallMeJ <19354902@qq.com>
2021-07-12 22:47:16 +0800

    test

--- WorkDD/src/com/hejie/acm/Num76.java
+++ WorkDD/src/com/hejie/acm/Num76.java
@@ -1,16 +1,30 @@
 package com.hejie.acm;
 
+import java.util.Arrays;
+
 public class Num76 {
     public static void main(String[] args) {
-        String name = "alex";
-        String typed = "aaleex";
-        System.out.println(isLongPressedName(name, typed));
+        String s1 = "abc";
+        String s2 = "bca";
+        System.out.println(CheckPermutation(s1, s2));
     }
 
-    public static boolean isLongPressedName(String name, String typed) {
-        boolean result = false;
-
-        return result;
+    public static boolean CheckPermutation(String s1, String s2) {
+        char[] s11 = s1.toCharArray();
+        char[] s22 = s2.toCharArray();
+        Arrays.sort(s11);
+        Arrays.sort(s22);
+        int len1 = s1.length();
+        int len2 = s2.length();
+        if (len1 != len2) {
+            return false;
+        }
+        for (int i = 0; i < s1.length(); i++) {
+            if (s11[i] != s22[i]) {
+                return false;
+            }
+        }
+        return true;
     }
 }
  
